58cc5d5714233deed8e9f5ed2afa31c34d6a25c6
Himanshu Yadav <himanshu.yadav@kelltontech.com>
2019-08-07 12:01:15 +0530

    Changes made for custom toast in android and iOS

--- android/src/main/java/com/azendoo/reactnativesnackbar/SnackbarModule.java
+++ android/src/main/java/com/azendoo/reactnativesnackbar/SnackbarModule.java
@@ -2,16 +2,20 @@ package com.azendoo.reactnativesnackbar;
 
 import android.graphics.Color;
 import android.os.Build;
-import com.google.android.material.snackbar.Snackbar;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Gravity;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,11 +25,12 @@ import java.util.Map;
 public class SnackbarModule extends ReactContextBaseJavaModule{
 
     private static final String REACT_NAME = "RNSnackbar";
-
+    private ReactApplicationContext context;
     private List<Snackbar> mActiveSnackbars = new ArrayList<>();
 
     public SnackbarModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.context = reactContext;
     }
 
     @Override
@@ -46,33 +51,38 @@ public class SnackbarModule extends ReactContextBaseJavaModule{
 
     @ReactMethod
     public void show(ReadableMap options, final Callback callback) {
-        ViewGroup view;
+        int duration = options.hasKey("duration") ? options.getInt("duration") : Toast.LENGTH_SHORT;
+        String message = options.hasKey("title") ? options.getString("title") : "";
+        LayoutInflater inflater = (LayoutInflater) this.context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+        // View toastview = inflater.inflate(R.layout.custom_toast, (ViewGroup) getCurrentActivity().findViewById(R.id.custom_toast_layout));
+        // View toastview = inflater.inflate(R.layout.custom_toast, (ViewGroup) getCurrentActivity().getWindow().getDecorView().findViewById(android.R.id.content));
+        View toastview = inflater.inflate(R.layout.custom_toast, (ViewGroup) getCurrentActivity().getWindow().getDecorView().findViewById(R.id.custom_toast_layout));
+        TextView toastTextView = (TextView) toastview.findViewById(R.id.custom_toast_text);
+        toastTextView.setText(message);
+        Toast toast = new Toast(this.context);
+        toast.setGravity(Gravity.BOTTOM, 0, 100);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(toastview);
+        toast.show();
+        // ViewGroup view;
+        // try {
+        //     view = (ViewGroup) getCurrentActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        //     return;
+        // }
 
-        try {
-            view = (ViewGroup) getCurrentActivity().getWindow().getDecorView().findViewById(android.R.id.content);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-
-        if (view == null) return;
-
-        mActiveSnackbars.clear();
-
-        if (!view.hasWindowFocus()) {
-            // The view is not focused, we should get all the modal views in the screen.
-            ArrayList<View> modals = recursiveLoopChildren(view, new ArrayList<View>());
-
-            for (View modal : modals) {
-                if (modal == null) continue;
-
-                displaySnackbar(modal, options, callback);
-            }
-
-            return;
-        }
-
-        displaySnackbar(view, options, callback);
+        // if (view == null) return;
+        // else {
+            // Toast toast = Toast.makeText(getReactApplicationContext(), message, duration);
+            // View view = toast.getView();
+            // if (options.hasKey("backgroundColor")) {
+            //     view.setBackgroundColor(options.getInt("backgroundColor"));
+            // }
+            // TextView text = (TextView) view.findViewById(android.R.id.message);
+            // text.setTextColor(Color.WHITE);
+            // toast.show();
+        // }
     }
 
     @ReactMethod
@@ -91,21 +101,18 @@ public class SnackbarModule extends ReactContextBaseJavaModule{
         int duration = options.hasKey("duration") ? options.getInt("duration") : Snackbar.LENGTH_SHORT;
 
         Snackbar snackbar = Snackbar.make(view, title, duration);
-        View snackbarView = snackbar.getView();
-        TextView snackbarText = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
-
         mActiveSnackbars.add(snackbar);
 
         // Set the background color.
         if (options.hasKey("backgroundColor")) {
-            snackbarView.setBackgroundColor(options.getInt("backgroundColor"));
+            snackbar.getView().setBackgroundColor(options.getInt("backgroundColor"));
         }
 
         if (options.hasKey("action")) {
             View.OnClickListener onClickListener = new View.OnClickListener() {
                 // Prevent double-taps which can lead to a crash.
                 boolean callbackWasCalled = false;
-
+                
                 @Override
                 public void onClick(View v) {
                     if (callbackWasCalled) return;
@@ -120,11 +127,11 @@ public class SnackbarModule extends ReactContextBaseJavaModule{
             snackbar.setActionTextColor(actionDetails.getInt("color"));
         }
 
-        if (options.hasKey("color")) {
-            snackbarText.setTextColor(options.getInt("color"));
-        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            // For older devices, explicitly set the text color; otherwise it may appear dark gray.
-            // http://stackoverflow.com/a/31084530/763231
+        // For older devices, explicitly set the text color; otherwise it may appear dark gray.
+        // http://stackoverflow.com/a/31084530/763231
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            View snackbarView = snackbar.getView();
+            TextView snackbarText = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
             snackbarText.setTextColor(Color.WHITE);
         }
 
@@ -151,3 +158,4 @@ public class SnackbarModule extends ReactContextBaseJavaModule{
     }
 
 }
+ 
--- ios/RNSnackBarView.m
+++ ios/RNSnackBarView.m
@@ -23,14 +23,12 @@ static const NSTimeInterval ANIMATION_DURATION = 0.250;
 {
     UILabel* titleLabel;
     UIButton* actionButton;
-    NSArray* horizontalLayoutConstraints;
+    UIImageView* toastImage;
     NSTimer* dismissTimer;
 }
 @property (nonatomic, strong) NSDictionary* pendingOptions;
-
 @property (nonatomic) RNSnackBarViewState state;
 @property (nonatomic, strong) NSString* title;
-@property (nonatomic, strong) UIColor* titleColor;
 @property (nonatomic, strong) NSString* actionTitle;
 @property (nonatomic, strong) UIColor* actionTitleColor;
 @property (nonatomic, strong) void (^pendingCallback)();
@@ -71,7 +69,7 @@ static const NSTimeInterval ANIMATION_DURATION = 0.250;
 
 - (instancetype)init
 {
-    self = [super initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 48, [UIScreen mainScreen].bounds.size.width, 48)];
+    self = [super initWithFrame:CGRectMake(16, [UIScreen mainScreen].bounds.size.height - 64, [UIScreen mainScreen].bounds.size.width - 32, 48)];
     if (self) {
         [self buildView];
     }
@@ -79,65 +77,64 @@ static const NSTimeInterval ANIMATION_DURATION = 0.250;
 }
 
 - (void)buildView {
-    CGFloat topPadding = 14;
+    CGFloat topPadding = 0;
     CGFloat bottomPadding = topPadding;
 
-    if (@available(iOS 11.0, *)) {
-        UIWindow *window = UIApplication.sharedApplication.keyWindow;
+    CGFloat rightmargin = [UIScreen mainScreen].bounds.size.width - 70;
 
-        if (window.safeAreaInsets.bottom > bottomPadding)
-            bottomPadding = window.safeAreaInsets.bottom;
-    }
+    // if (@available(iOS 11.0, *)) {
+    //     UIWindow *window = UIApplication.sharedApplication.keyWindow;
+
+    //     if (window.safeAreaInsets.bottom > bottomPadding)
+    //         bottomPadding = window.safeAreaInsets.bottom;
+    // }
 
     self.backgroundColor = [UIColor colorWithRed:0.196078F green:0.196078F blue:0.196078F alpha:1.0F];
     self.accessibilityIdentifier = @"snackbar";
-
+  
     titleLabel = [UILabel new];
     titleLabel.text = _title;
     titleLabel.numberOfLines = 2;
-    titleLabel.textColor = _titleColor;
-    titleLabel.font = [UIFont boldSystemFontOfSize:14];
+    titleLabel.textColor = [UIColor colorWithRed:29/255 green:45/255 blue:61/255 alpha:1.0F];
+    titleLabel.font = [UIFont boldSystemFontOfSize:12];
     [titleLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self addSubview:titleLabel];
+    
+    toastImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"sucessIcon"]];
+    toastImage.contentMode = UIViewContentModeScaleAspectFit;
+    toastImage.frame = CGRectMake(0, 0, 24, 24);
+    toastImage.center = CGPointMake(20,self.frame.size.height / 2);
 
-    actionButton = [UIButton new];
-    actionButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
-    [actionButton setTitle:@"" forState:UIControlStateNormal];
-    [actionButton addTarget:self action:@selector(actionPressed:) forControlEvents:UIControlEventTouchUpInside];
-    [actionButton setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self addSubview:actionButton];
 
+//    actionButton = [UIButton new];
+//    actionButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
+//    [actionButton setTitle:@"" forState:UIControlStateNormal];
+    // [actionButton addTarget:self action:@selector(actionPressed:) forControlEvents:UIControlEventTouchUpInside];
+//    [actionButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self addSubview:toastImage];
+//    [self addSubview:actionButton];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:
+          @"H:|-40-[titleLabel]-24-|"
+          options:0 metrics:nil views:@{@"titleLabel": titleLabel}]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:[NSString stringWithFormat:@"V:|-%f-[titleLabel]-%f-|", topPadding, bottomPadding] options:0 metrics:nil views:@{@"titleLabel": titleLabel}]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:actionButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:titleLabel attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
+
+//     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:[NSString stringWithFormat: @"H:|-10-[toastImage]-%f-|", rightmargin]
+//          options:0 metrics:nil views:@{@"toastImage": toastImage}]];
+
+//    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-10-[toastImage]-10-|" options:0 metrics:nil views:@{@"toastImage": toastImage}]];
+
+
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:toastImage attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:titleLabel attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
     [titleLabel setContentCompressionResistancePriority:250 forAxis:UILayoutConstraintAxisHorizontal];
     [titleLabel setContentHuggingPriority:250 forAxis:UILayoutConstraintAxisHorizontal];
-    [actionButton setContentCompressionResistancePriority:750 forAxis:UILayoutConstraintAxisHorizontal];
-    [actionButton setContentHuggingPriority:750 forAxis:UILayoutConstraintAxisHorizontal];
+    [toastImage setContentCompressionResistancePriority:750 forAxis:UILayoutConstraintAxisHorizontal];
+    [toastImage setContentHuggingPriority:750 forAxis:UILayoutConstraintAxisHorizontal];
 
-    self.actionHidden = YES;
 }
 
 -(void)setTitle:(NSString *)title {
     titleLabel.text = title;
-}
-
--(void)setActionHidden:(BOOL)hidden {
-    if (actionButton.hidden != hidden || horizontalLayoutConstraints == nil) {
-        actionButton.hidden = hidden;
-        if (horizontalLayoutConstraints != nil) {
-            [self removeConstraints:horizontalLayoutConstraints];
-        }
-        if (hidden) {
-            horizontalLayoutConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-24-[titleLabel]-24-|" options:0 metrics:nil views:@{@"titleLabel": titleLabel}];
-        } else {
-            horizontalLayoutConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-24-[titleLabel]-24-[actionButton]-24-|" options:0 metrics:nil views:@{@"titleLabel": titleLabel, @"actionButton": actionButton}];
-        }
-        [self addConstraints:horizontalLayoutConstraints];
-    }
-}
-
--(void)setTitleColor:(UIColor *)titleColor {
-    titleLabel.textColor = titleColor;
 }
 
 -(void)setActionTitle:(NSString *)actionTitle {
@@ -159,17 +156,18 @@ static const NSTimeInterval ANIMATION_DURATION = 0.250;
     UIWindow* keyWindow = [UIApplication sharedApplication].keyWindow;
     [keyWindow addSubview:self];
     [self setTranslatesAutoresizingMaskIntoConstraints:false];
-    [keyWindow addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[self(>=48)]|" options:0 metrics:nil views:@{@"self": self}]];
-    [keyWindow addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[self]|" options:0 metrics:nil views:@{@"self": self}]];
+    [keyWindow addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[self(>=48)]-(16)-|" options:0 metrics:nil views:@{@"self": self}]];
+    // [keyWindow addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[self(>=48)]|" options:0 metrics:nil views:@{@"self": self}]];
+    [keyWindow addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(16)-[self]-(16)-|" options:0 metrics:nil views:@{@"self": self}]];
 
     self.transform = CGAffineTransformMakeTranslation(0, self.bounds.size.height);
     titleLabel.alpha = 0;
-    actionButton.alpha = 0;
+    toastImage.alpha = 0;
     self.state = RNSnackBarViewStatePresenting;
     [UIView animateWithDuration:ANIMATION_DURATION animations:^{
         self.transform = CGAffineTransformIdentity;
         titleLabel.alpha = 1;
-        actionButton.alpha = 1;
+        toastImage.alpha = 1;
      } completion:^(BOOL finished) {
         self.state = RNSnackBarViewStateDisplayed;
         NSTimeInterval interval;
@@ -204,45 +202,62 @@ static const NSTimeInterval ANIMATION_DURATION = 0.250;
 
 - (void)show {
     if (self.state == RNSnackBarViewStateDisplayed || self.state == RNSnackBarViewStatePresenting) {
-        [self dismiss];
-        return;
+      [self dismiss];
+      return;
     }
     if (self.state == RNSnackBarViewStateDismissing) {
-        return;
+      return;
     }
-    if (!_pendingOptions) {
-        return;
-    }
+    if (!_pendingOptions) { return; }
 
-    id backgroundColor = _pendingOptions[@"backgroundColor"];
-    self.backgroundColor = backgroundColor
-        ? [RCTConvert UIColor:backgroundColor]
-        : [UIColor colorWithRed:0.196078F green:0.196078F blue:0.196078F alpha:1.0F];
-
-    id titleColor =_pendingOptions[@"color"];
-    self.titleColor = titleColor
-        ? [RCTConvert UIColor:titleColor]
-        : [UIColor whiteColor];
-
+    self.backgroundColor = [UIColor whiteColor];//backgroundColor ? [RCTConvert UIColor:backgroundColor] : [UIColor colorWithRed:0.196078F green:0.196078F blue:0.196078F alpha:1.0F];
     self.title = _pendingOptions[@"title"];
     self.callback = _pendingCallback;
-
     NSDictionary* action = _pendingOptions[@"action"];
     if (action) {
         self.actionTitle = _pendingOptions[@"action"][@"title"];
-        self.actionHidden = _pendingOptions[@"action"][@"title"] ? NO : YES;
         NSNumber* color = _pendingOptions[@"action"][@"color"];
         self.actionTitleColor = [RCTConvert UIColor:color];
     } else {
         self.actionTitle = @"";
-        self.actionHidden = YES;
     }
-
-    NSNumber* duration = _pendingOptions[@"duration"]
-        ? (NSNumber*)_pendingOptions[@"duration"]
-        : @(-1);
-
+    NSNumber* duration = _pendingOptions[@"duration"] ? (NSNumber*)_pendingOptions[@"duration"] : @(-1);
+    [self setStyle];
     [self presentWithDuration:duration];
+    
 }
 
+-(void)setStyle {
+    self.layer.cornerRadius = 5.0;
+    self.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.layer.shadowOpacity = 0.5;
+    self.layer.shadowOffset = CGSizeZero;
+    self.layer.shadowRadius = 3;
+//    self.layer.borderWidth = 1.0;
+//    self.layer.borderColor = [UIColor colorWithRed:0.196078F green:0.196078F blue:0.196078F alpha:1.0F].CGColor;
+//    actionButton.backgroundColor = [UIColor redColor];
+//    [actionButton setImage:[UIImage imageNamed:@"sucessIcon"] forState:UIControlStateNormal];
+    NSInteger type = 2;
+    if(_pendingOptions[@"type"] != nil) {
+        type = [[_pendingOptions objectForKey:@"type"] integerValue];
+    }
+    switch (type) {
+        case 0:
+            toastImage.image = [UIImage imageNamed:@"sucessIcon"];
+            break;
+        case 1:
+            toastImage.image = [UIImage imageNamed:@"warningIcon"];
+            break;
+        case 2:
+            toastImage.image = [UIImage imageNamed:@"infoIcon"];
+            break;
+        case 3:
+            toastImage.image = [UIImage imageNamed:@"failIcon"];
+            break;
+        default:
+            toastImage.image = [UIImage imageNamed:@"warningIcon"];
+            break;
+    }
+    
+}
 @end
